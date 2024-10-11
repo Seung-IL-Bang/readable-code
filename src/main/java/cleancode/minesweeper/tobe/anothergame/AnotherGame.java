48936e653550ad51e41161df487c4d04377b1ec3
Seungil <seungilbang@khu.ac.kr>
2024-10-12 02:38:41 +0900

    패키지 나누기

---
+++ src/main/java/cleancode/minesweeper/tobe/anothergame/AnotherGame.java
@@ -0,0 +1,10 @@
+package cleancode.minesweeper.tobe.anothergame;
+
+import cleancode.minesweeper.tobe.game.GameRunnable;
+
+public class AnotherGame implements GameRunnable {
+    @Override
+    public void run() {
+        // game run...
+    }
+}
